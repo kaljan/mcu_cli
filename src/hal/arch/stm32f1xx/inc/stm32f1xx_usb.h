9841713d03cf2673f7ee5a10a4449ac8f55fdf6f
kaljan <kaljan.nothern@gmail.com>
2024-05-07 21:45:10 +0300

    Add stm32f1xx_usb source

---
+++ src/hal/arch/stm32f1xx/inc/stm32f1xx_usb.h
@@ -0,0 +1,25 @@
+/**
+ * @file    stm32f1xx_usb.h
+ * @author  Mikalai Naurotski (kaljan@nothern.com)
+ * @version 0.0.0
+ * @date    2024-04-28
+ *
+ * @brief
+ */
+#ifndef STM32F1XX_USB_H
+#define STM32F1XX_USB_H
+
+#include "stm32f1xx_config.h"
+#include "stm32f1xx_rcc.h"
+#include "stm32f1xx_gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* STM32F1XX_USB_H */
